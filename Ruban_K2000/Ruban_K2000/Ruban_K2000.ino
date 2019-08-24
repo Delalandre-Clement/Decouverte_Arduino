d9a82d19fd127f64f65faa50066b692fdfcf843b
Delalandre Clément <33206273+Delalandre-Clement@users.noreply.github.com>
2019-08-24 18:26:53 +0200

    Programme K2000
    Allumage et extinction des LED en ruban

--- Ruban_K2000/Ruban_K2000/Ruban_K2000.ino
+++ Ruban_K2000/Ruban_K2000/Ruban_K2000.ino
@@ -1,11 +1,38 @@
-int postion_LED[7}={22,23,24,25,26,27,28};
+/* Initialisation des varirables globales */
+int positionLed[7]={22,23,24,25,26,27,28};
+int delaiAttente=100;
 
 void setup() {
-  // put your setup code here, to run once:
-
+  /*Pin en mode Sortie courant */
+  for(int nombreTour = 0; nombreTour <= 6; nombreTour++){
+    pinMode(positionLed[nombreTour],OUTPUT);
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  /* Boucle d'allumage des LED dans le sens positif */
+  for(int nombreTour = 0; nombreTour <= 6; nombreTour++){
+    /* Allumage de la LED actuelle en fonction du nombre de tour dans la boucle */
+    digitalWrite(positionLed[nombreTour],HIGH); 
+    delay(delaiAttente);
+    /*Extinction de la LED-1 sauf si on est à la première LED */
+    if(nombreTour != 0){
+      digitalWrite(positionLed[nombreTour-1],LOW);
+    }
+    /*Extinction de la LED actuelle */
+    digitalWrite(positionLed[nombreTour],LOW);
+  }
 
+  /* Boucle d'allumage des LED dans le sens négatif */
+  for(int nombreTour = 6; nombreTour > 0; nombreTour--){
+    /* Allumage de la LED actuelle en fonction du nombre de tour dans la boucle */
+    digitalWrite(positionLed[nombreTour],HIGH); 
+    /*Extinction de la LED+1 sauf si on est à la dernière LED */
+    if(nombreTour != 6){
+      delay(delaiAttente);
+      digitalWrite(positionLed[nombreTour+1],LOW);
+    }
+    /*Extinction de la LED actuelle */
+    digitalWrite(positionLed[nombreTour],LOW);
+  }
 }
